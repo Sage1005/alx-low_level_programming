34b4fcb9715c4f3a9e63f758910e23f7b29625c8
Sage1005 <bamgbala.wasiu@gmail.com>
2022-09-08 16:35:48 -0700

    Write a C program that prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *main - Entry point 
+ * main - Entry point
  *
- * Description: "programming is like bullying a multilingual puzzle
- *Return: 0 if exited and non-zero if otherwise 
+ * Description: "programming is like bullying a multilingual puzzle"
+ * Return: 0 if exited and non-zero if otherwise
  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
