fe0ec7f059ccedfbc243635cc9b1ce8a1dea44bc
Sage1005 <bamgbala.wasiu@gmail.com>
2022-09-12 01:54:22 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,18 +3,19 @@
 /**
  * main - Prints all possiblecombinations of single-digit numbers
  *
+ * Description: it prints all possible
+ * combination of single digits
  * Return: Always 0
  */
 int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num < 10; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-
 		putchar(',');
 		putchar(' ');
 	}
