c42d7ac7068c4eb3935bafc54e86eb4dd39b6d1c
Sage1005 <bamgbala.wasiu@gmail.com>
2022-09-13 17:59:43 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,12 +12,15 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for(j = 0; j < 26; j++)
+		for (j = 0; j < 26; j++)
 		{
 			_putchar(c + j);
 		}
 		_putchar(10);
 	}
 }
+	
+
+
 	
 
