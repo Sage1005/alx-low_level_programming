db579e393d320b11a12b49a6a77fe51cffe68761
Sage1005 <bamgbala.wasiu@gmail.com>
2022-09-15 13:52:37 -0700

    done

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,77 +1,23 @@
-#include <stdlib.h>                                                                                                                                          
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
-#include <time.h>                                                                                                                                            
+/**
+ * main - Entry point
+ * REturn: Always 0 (success)
+ */
+void positive_or_negative(int i)
+{
+	srand(time(0));
 
-#include <stdio.h>                                                                                                                                           
+	i = rand() - RAND_MAX / 2;
 
-                                                                                                                                                             
+	/* your code goes there */
 
-/**                                                                                                                                                          
- *
- *  * main - Prints randon number and states whether                                                                                                            
- *
- *   * it is positive, negative, or zero                                                                                                                         
- *
- *    *                                                                                                                                                           
- *
- *     * Return: Always 0                                                                                                                                          
- *
- *      */                                                                                                                                                          
-
-int main(void)                                                                                                                                               
-
-{                                                                                                                                                            
-
-	        int n;                                                                                                                                               
-
-		                                                                                                                                                             
-
-		        srand(time(0));                                                                                                                                      
-
-			        n = rand() - RAND_MAX / 2;                                                                                                                           
-
-				                                                                                                                                                             
-
-				        if (n > 0)                                                                                                                                           
-
-						                printf("%d is positive\n", n);                                                                                                               
-
-					        else if (n < 0)                                                                                                                                      
-
-							                printf("%d is negative\n", n);                                                                                                               
-
-						        else                                                                                                                                                 
-
-								                printf("%d is zero\n", n);                                                                                                                   
-
-							                                                                                                                                                             
-
-							        return (0);                                                                                                                                          
-
-}                                            #include <stdlib.h>                                                                                                                                          
-
-#include <time.h>                                                                                                                                      
-#include <stdio.h>                                                                                                                                        main - Prints randon number and states whether                                               
-  it is positive, negative, or zero                                                                                                                  * Return: Always 0                                                                                                                                          
- *
- *      */                                                                                                                                                          
-
-int main(void)                                                                                                                                               
-
-{                                                                                                                                                            int n;
-
-		                                                                                                                                                             
-
-		        srand(time(0));                                                                                                                                      
-
-			        n = rand() - RAND_MAX / 2;                                                                                                                           
-
-				                                                                                                                                                             
-
-				        if (n > 0)                                                                                                                                           
-
-						                printf("%d is positive\n", n);                                                                      
-					        else if (n < 0)                                                                                                                                    printf("%d is negative\n", n); 
-						        else                                                                                                                                          printf("%d is zero\n", n);     return (0);                                                                                                                                          
-
-}                                            
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else 
+		printf("%d is negative\n", i);
+}
