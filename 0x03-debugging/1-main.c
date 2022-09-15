3944b14de5f7aaffb62c72e6386ab708d256976b
Sage1005 <bamgbala.wasiu@gmail.com>
2022-09-15 11:56:32 -0700

    done

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,26 +1,2 @@
 #include <stdio>
-void print_diagonal(int a);
 
-int main(void)
-{
-	print_diagonal(10);
-	return (0);
-}
-void print_diagonal(int a)
-{
-	int i;
-
-	i = 1;
-	while (i <= a)
-	{
-		for (int k = 0; k <= a-i; k++)
-		{
-			if (k != 0)
-			{
-				printf("h");
-			}
-		}
-		printf("\\\n");
-		i++;
-	}
-:}
