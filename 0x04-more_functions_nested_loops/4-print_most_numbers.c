101e695e410ef075c7637d97c662fc7bbaca9b36
Sage1005 <bamgbala.wasiu@gmail.com>
2022-09-17 07:21:29 -0700

    done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,6 @@
 /**
  * print_most_numbers - Prints numbers "0" to "9", except 2 and 4
  *
- *
  * Return: is zero
  */
 void print_most_numbers(void)
@@ -12,7 +11,7 @@ void print_most_numbers(void)
 
 	for (digits = '0'; digits <= '9'; digits++)
 	{
-		if (digits != '4')
+		if (digits != '9' && digits != '4')
 		{
 			_putchar(digits);
 		}
