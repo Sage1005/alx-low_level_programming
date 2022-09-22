53fe9740a818c732952b572efac8210a463ba96e
Sage1005 <bamgbala.wasiu@gmail.com>
2022-09-22 14:52:16 -0700

    done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,14 @@
 char *rot13(char *c)
 {
 	int i, j;
-	char rot_13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+	char rot_13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (alphabet[j] == c[i])
+			if (; alphabet[j] == c[i])
 			{
 				c[i] = rot_13[j];
 				break;
