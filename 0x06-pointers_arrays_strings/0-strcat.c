07f7c26a02a27026a796d30f1355dc8fe423440d
Sage1005 <bamgbala.wasiu@gmail.com>
2022-09-22 04:24:11 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,21 +2,19 @@
 
 /**
  * _strcat - function to concatenate two strings and return a new string
- * with n elements of the appended string
  *
  * @dest: string 1
  * @src: string 2
- * @n: number of chars to include from appended string
  * Return: char pointer
  */
-char *_strcat(char *dest, char *src; int n)
+char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 	char *p;
 
 	while (*(dest + i) != '\0')
 		i++;
-	while (!(*(src + j) == '\0' || j == n))
+	while (!(*(src + j) != '\0')
 	{
 		*(dest + i) = *(src + j);
 		i++;
